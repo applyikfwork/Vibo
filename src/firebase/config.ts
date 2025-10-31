da000ff7b74bf4420effcee637c2b82813030a8c
Discountedmarket <discountedmarkets@gmail.com>
2025-10-31 04:53:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-165909115-9ff76",
+  "appId": "1:661591162563:web:768da4cd05482577e06c76",
+  "apiKey": "AIzaSyD1fuKYl1N-KfvJ7zKFF1twNrqs_nss8QQ",
+  "authDomain": "studio-165909115-9ff76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "661591162563"
+};
